306ac4692b88b405c143f94b7e3e3b28ac59c5ab
vidhi220107-cloud <vidhi220107@gmail.com>
2026-04-25 23:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,9 +150,9 @@ if detect:
     st.subheader("📊 Detection Results")
     
    if status == "Normal":
-    st.success("🟢 Normal Traffic Detected")
-else:
-    st.error("🔴 Attack Detected! Immediate Action Required")
+       st.success("🟢 Normal Traffic Detected")
+   else:
+       st.error("🔴 Attack Detected! Immediate Action Required")
 
     c1, c2, c3 = st.columns(3)
 
